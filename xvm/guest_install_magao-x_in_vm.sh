b906ff32b5447cf1a85b8dc3b37daa21f8df73f0
Joseph Long <github@joseph-long.com>
2024-08-27 22:46:06 -0400

    Update install_MagAOX.sh path

--- xvm/guest_install_magao-x_in_vm.sh
+++ xvm/guest_install_magao-x_in_vm.sh
@@ -8,4 +8,4 @@ set -x
 sudo chmod g+w /opt/MagAOX/source
 sudo chown :magaox-dev /opt/MagAOX/source
 git clone https://github.com/magao-x/MagAOX.git /opt/MagAOX/source/MagAOX
-bash -lx /opt/MagAOX/source/MagAOX/setup/steps/install_MagAOX.sh || exit 1
+bash -lx ~/magao-x-setup/steps/install_MagAOX.sh || exit 1
