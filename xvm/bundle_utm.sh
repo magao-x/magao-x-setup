db9f61b68a58084335d7b2851165a9277e53307a
Joseph Long <github@joseph-long.com>
2025-03-25 10:54:43 -0400

    Use parallel gzip in bundling

--- xvm/bundle_utm.sh
+++ xvm/bundle_utm.sh
@@ -10,4 +10,9 @@ ls -R ./output/bundle/MagAO-X.utm
 mv ./output/xvm.qcow2 ./output/bundle/MagAO-X.utm/Data/xvm.qcow2
 cp ./output/xvm_key ./output/xvm_key.pub ./output/bundle/
 cd ./output/bundle/
-tar -cJvf ./MagAO-X_UTM_${vmArch}.tar.xz ./*
+if [[ $(uname -o) == Darwin ]]; then
+    nCpu=$(sysctl -n hw.ncpu)
+else
+    nCpu=$(nproc)
+fi
+tar -cvf - ./* | pigz -7 -p $nCpu > ./MagAO-X_UTM_${vmArch}.tar.gz
