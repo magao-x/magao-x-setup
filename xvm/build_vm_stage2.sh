54ac8c821a8badca3458e558cf805bf11e992831
Joseph Long <github@joseph-long.com>
2025-03-25 10:59:39 -0400

    Typo fixes / reordering

--- xvm/build_vm_stage2.sh
+++ xvm/build_vm_stage2.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 echo "Starting up the VM for MagAO-X dependencies installation..."
 source ./_common.sh
-if [[ -e ./output/xvm_stage2.qcow2 ]]; then
+if [[ -e ./output/xvm_stage1.qcow2 ]]; then
     cp ./output/xvm_stage1.qcow2 ./output/xvm.qcow2
 elif [[ ! -e ./output/xvm.qcow2 ]]; then
     echo "No existing xvm.qcow2 found to use in stage 2"
     exit 1
 fi
 $qemuSystemCommand || exit 1 &
+echo "Waiting 60 sec for VM to become ready..."
 sleep 60
+echo "Updating guest repo checkout"
 updateGuestRepoCheckout  # since the previous stage VM may be from cache
+echo "Provisioning up to MagAOX build"
 ssh -p 2201 -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking=no" -i ./output/xvm_key xdev@localhost 'bash -s' < ./guest_provision_up_to_build.sh
 # wait for the backgrounded qemu process to exit:
 wait
