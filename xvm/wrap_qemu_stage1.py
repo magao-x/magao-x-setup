6d91aab918b0695fd99cb2dc1bdd12416d663492
Joseph Long <github@joseph-long.com>
2025-03-26 12:54:56 -0400

    I dunno man

--- xvm/wrap_qemu_stage1.py
+++ xvm/wrap_qemu_stage1.py
@@ -5,19 +5,19 @@ print(sys.argv)
 
 # Start QEMU as a subprocess
 proc = subprocess.Popen(sys.argv[1:],
-                        stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=False)
+                        stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
 
 while True:
     line = proc.stdout.readline()
     if not line:
         break  # QEMU exited
 
-    sys.stdout.write(repr(line))  # Print output to console
+    sys.stdout.write(repr(line))
 
-    if b"Test this media" in line:  # Change this to your specific trigger string
+    if "Test this media" in line:
         print("Detected boot prompt! Sending keys...")
         time.sleep(0.1)
-        proc.stdin.write(b"sendkey up\n")
+        proc.stdin.write("sendkey up\n")
         time.sleep(0.1)
-        proc.stdin.write(b"sendkey ret\n")
+        proc.stdin.write("sendkey ret\n")
         proc.stdin.flush()
