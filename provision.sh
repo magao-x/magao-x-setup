fe349445bdef4a9f3579b00a1c13b4c6aa1396d3
Joseph Long <github@joseph-long.com>
2025-06-24 19:03:25 -0400

    set x

--- provision.sh
+++ provision.sh
@@ -2,7 +2,7 @@
 set -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/_common.sh
-
+set -x
 # CentOS + devtoolset-7 aliases sudo, but breaks command line arguments for it,
 # so if we need those, we must use $_REAL_SUDO.
 if [[ -e /usr/bin/sudo ]]; then
