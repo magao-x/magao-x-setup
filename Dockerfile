e7b8fa1dba141258eaf64cc591ebc3a39477d305
Joseph Long <github@joseph-long.com>
2025-06-24 20:12:41 -0400

    Fix up container build, switch cfitsio url to https

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM rockylinux/rockylinux:9-ubi-init
 ENV MAGAOX_ROLE=container
 RUN echo "MAGAOX_ROLE=${MAGAOX_ROLE}" > /etc/profile.d/magaox_role.sh
 ADD ./_common.sh /setup/
+RUN dnf install -y 'dnf-command(config-manager)' sudo
 ADD ./steps/install_rocky_9_packages.sh /setup/steps/
 RUN bash /setup/steps/install_rocky_9_packages.sh
 ADD ./setup_users_and_groups.sh /setup/
--- steps/install_cfitsio.sh
+++ steps/install_cfitsio.sh
@@ -8,7 +8,7 @@ CFITSIO_VERSION="4.6.2"
 # CFITSIO
 #
 if [[ ! -d ./cfitsio-$CFITSIO_VERSION ]]; then
-    _cached_fetch http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-$CFITSIO_VERSION.tar.gz cfitsio-$CFITSIO_VERSION.tar.gz
+    _cached_fetch https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-$CFITSIO_VERSION.tar.gz cfitsio-$CFITSIO_VERSION.tar.gz
     tar xzf cfitsio-$CFITSIO_VERSION.tar.gz || exit 1
 fi
 cd cfitsio-$CFITSIO_VERSION || exit 1
