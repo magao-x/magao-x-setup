8fa88885ed349de08b19569da3dacfe47ab5f2e7
Joseph Long <github@joseph-long.com>
2025-08-16 17:13:46 -0400

    Typo fix

--- steps/configure_ldap_auth.sh
+++ steps/configure_ldap_auth.sh
@@ -5,7 +5,7 @@ set -uo pipefail
 
 sudo dnf install -y sssd sssd-tools oddjob-mkhomedir || exit_with_error "Unable to install OS packages for LDAP authentication"
 sudo systemctl enable --now oddjobd || true
-sudo authselect select sssd --force mkhomedir || exit_with_error "Unable to select SSSD for auth"
+sudo authselect select sssd --force with-mkhomedir || exit_with_error "Unable to select SSSD for auth"
 sudo sssctl config-check || exit_with_error "Config check failed for SSSD"
 sudo systemctl enable --now sssd || true
 echo 'AuthorizedKeysCommand /usr/bin/sss_ssh_authorizedkeys' | sudo tee /etc/ssh/sshd_config.d/99-sssd.conf
