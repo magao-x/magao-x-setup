b756d89da39b0c07eb360c333cdc0ba254dac124
Joseph Long <github@joseph-long.com>
2025-09-20 14:23:02 -0400

    Update config from working copy on AOC

--- jupyterhub_config_minimal.py
+++ jupyterhub_config_minimal.py
@@ -6,13 +6,16 @@ c = get_config()  #noqa
 #          Default is two weeks.
 #  Default: 14
 c.JupyterHub.cookie_max_age_days = 28
-
 # Unprivileged jupyterhub needs help to spawn per-user servers
-c.JupyterHub.spawner_class = 'sudospawner.SudoSpawner'
-
+c.JupyterHub.spawner_class = "systemd"
+# Per-user runtime dir with templated values
+c.SystemdSpawner.runtime_dir = '/run/user/{uid}/jupyter-singleuser'
+# JupyterHub launched by SystemD gets read-only view of /etc/jupyterhub,
+# requiring configuration of PID file location.
+c.ConfigurableHTTPProxy.pid_file = '/run/jupyterhub/jupyterhub-proxy.pid'
+# SystemD unit ensures creation of this dir for persistent state
+c.JupyterHub.db_url = 'sqlite:////var/lib/jupyterhub/jupyterhub.sqlite'
 # Only specified users may log in to JupyterHub
 c.PAMAuthenticator.allowed_groups = {'jupyterhub', 'xwcl-admin'}
-
 # Which groups should get admin access
 c.PAMAuthenticator.admin_groups = {'wheel', 'xwcl-admin'}
-
