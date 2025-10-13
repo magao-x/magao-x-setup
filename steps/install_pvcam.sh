afbe1989a6590c9f3e6b63e8ef480ca8b0c6e9ab
Joseph Long <github@joseph-long.com>
2025-10-13 13:16:24 -0400

    Update PVCam and SDK installer

--- steps/install_pvcam.sh
+++ steps/install_pvcam.sh
@@ -16,16 +16,28 @@ if [[ ! -e /opt/pvcam/etc/profile.d/pvcam.sh ]]; then
     echo 'y\nn\n' | bash ./$PVCAM_RUNFILE || exit_with_error "Couldn't install Teledyne Photometrics PVCam for Kinetix"
     log_success "Ran Teledyne Photometrics PVCam installer for Kinetix"
 else
-    log_info "Existing PVCam install found, use the originall installer or /opt/pvcam/pvcam.uninstall.sh to remove"
+    log_info "Existing PVCam install found, use the original installer or /opt/pvcam/pvcam.uninstall.sh to remove"
 fi
 
 PVCAMSDK_RUNFILE=pvcam-sdk_3.10.0.3-1.run
 cd /opt/MagAOX/vendor/teledyne/pvcam-sdk || exit 1
 
 chmod +x ./$PVCAMSDK_RUNFILE
-if [[ ! -e /opt/pvcam/etc/profile.d/pvcam.sh ]]; then
-    echo 'y\nn\n' | bash ./$PVCAMSDK_RUNFILE || exit_with_error "Couldn't install Teledyne Photometrics PVCam for Kinetix"
-    log_success "Ran Teledyne Photometrics PVCam installer for Kinetix"
+if [[ ! -e /opt/pvcam/sdk ]]; then
+    bash ./$PVCAMSDK_RUNFILE -q -- -q || exit_with_error "Couldn't install Teledyne Photometrics PVCam SDK for Kinetix"
+    log_success "Ran Teledyne Photometrics PVCam SDK installer for Kinetix"
 else
-    log_info "Existing PVCam install found, use the originall installer or /opt/pvcam/pvcam.uninstall.sh to remove"
+    log_info "Existing PVCam SDK install found"
+fi
+
+log_info "Changing SELinux context of pvcam_pcie.ko..."
+chcon -t modules_object_t /opt/pvcam/drivers/in-kernel/pcie/pvcam_pcie.ko || exit 1
+log_info "Done!"
+
+if [ ! -f /etc/modules-load.d/pvcam_pcie.conf ]; then
+    log_info "Adding autoload file for pvcam_pcie to /etc/modules-load.d"
+    echo pvcam_pcie > /etc/modules-load.d/pvcam_pcie.conf || exit_with_error "Can't make autoload file"
+    log_info "Done!"
+else
+    log_info "Found autoload file for pvcam_pcie in /etc/modules-load.d"
 fi
