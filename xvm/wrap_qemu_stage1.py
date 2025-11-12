e2596753e37ad297a11756c3fe45e3516c531402
Joseph Long <github@joseph-long.com>
2025-11-12 17:17:18 -0500

    Read the stream as text instead of bytes

--- xvm/wrap_qemu_stage1.py
+++ xvm/wrap_qemu_stage1.py
@@ -12,49 +12,44 @@ temp.bind(("", 0))
 qemuPort = temp.getsockname()[1]
 temp.close()
 
+# Use that port number to launch QEMU with a monitor socket listening
 proc = subprocess.Popen(
     sys.argv[1:] + ["-monitor", f"tcp:localhost:{qemuPort},server,nowait"],
     stdin=subprocess.PIPE,
     stdout=subprocess.PIPE,
     stderr=subprocess.STDOUT,
-    text=False,
+    text=True,
+    encoding='utf8',
 )
 
-sock = None
-last_line = b''
-skipped = False
+# Connect to the monitor socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(("localhost", qemuPort))
+print(f"Connected to localhost:{qemuPort}")
 
 while True:
-    line = proc.stdout.read(128)
+    line = proc.stdout.readline()
     if not line:
         break  # QEMU exited
 
-    print(repr(line).encode('utf8'))
+    print(line)
     sys.stdout.buffer.flush()
 
-    if sock is None:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(("localhost", qemuPort))
-        print(f"Connected to localhost:{qemuPort}")
-
-    full_line = last_line + line
-    if b"Test this media" in full_line:
+    if "Test this media" in line:
         print("Detected boot prompt! Sending keys...", file=sys.stderr)
         time.sleep(0.1)
         sock.sendall(b"sendkey up\n")
         time.sleep(0.1)
         sock.sendall(b"sendkey ret\n")
         last_line = b''
-    elif b"Press [Esc] to abort check." in full_line:
+    elif "Press [Esc] to abort check." in line:
         print("Detected media integrity prompt! Sending keys...", file=sys.stderr)
         time.sleep(0.1)
         sock.sendall(b"sendkey esc\n")
         time.sleep(0.5)
         sock.sendall(b"sendkey esc\n")
         last_line = b''
-    elif b"Checking:" in full_line:
+    elif "Checking:" in line:
         print("Somehow it's still going.")
         proc.kill()
         sys.exit(1)
-    else:
-        last_line = line
