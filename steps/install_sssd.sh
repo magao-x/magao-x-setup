28dc9d21c03d368dff242bd78598e574c4529bfc
Joseph D. Long <me@joseph-long.com>
2025-11-14 15:53:09 -0500

    Add oddjob-mkhomedir to install

--- steps/install_sssd.sh
+++ steps/install_sssd.sh
@@ -7,7 +7,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 
 log_info "Installing SSSD for authentication with remote LDAP server..."
-dnf install -y sssd || exit 1
+dnf install -y sssd oddjob-mkhomedir || exit_with_error "Failed to install distro packages for SSSD or mkhomedir"
 log_info "Selecting SSSD as the auth backend"
-authselect select sssd --force with-mkhomedir || exit 1
+authselect select sssd --force with-mkhomedir || exit_with_error "Failed to select SSSD with authselect"
 log_success "SSSD selected!"
+systemctl enable --now oddjobd.service || exit_with_error "Couldn't enable/start oddjobd"
+log_success "oddjobd enabled!"
