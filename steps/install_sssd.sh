a6aa3eef4500613e92e0671b3831e4fb97a4ad4f
Joseph D. Long <me@joseph-long.com>
2025-11-14 15:47:52 -0500

    Add step for sssd installation, not currently run by default

---
+++ steps/install_sssd.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+if [[ "$EUID" != 0 ]]; then
+    sudo -H bash $0 "$@"
+    exit $?
+fi
+log_info "Installing SSSD for authentication with remote LDAP server..."
+dnf install -y sssd || exit 1
+log_info "Selecting SSSD as the auth backend"
+authselect select sssd --force mkhomedir || exit 1
+log_success "SSSD selected!"
