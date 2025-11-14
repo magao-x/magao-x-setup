e280277b1f49a8f43eff1ba8e75a505fe1fcb44a
Joseph D. Long <me@joseph-long.com>
2025-11-14 15:50:44 -0500

    Syntax fix

--- steps/install_sssd.sh
+++ steps/install_sssd.sh
@@ -3,8 +3,11 @@ if [[ "$EUID" != 0 ]]; then
     sudo -H bash $0 "$@"
     exit $?
 fi
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/../_common.sh
+
 log_info "Installing SSSD for authentication with remote LDAP server..."
 dnf install -y sssd || exit 1
 log_info "Selecting SSSD as the auth backend"
-authselect select sssd --force mkhomedir || exit 1
+authselect select sssd --force with-mkhomedir || exit 1
 log_success "SSSD selected!"
