1ff4d8288304e33c54ffce3e2e88b2cf1ada680c
Joseph D. Long <me@joseph-long.com>
2025-11-24 17:15:28 -0300

    Fix up Fedora CUDA install script

--- steps/install_cuda_fedora_42.sh
+++ steps/install_cuda_fedora_42.sh
@@ -11,7 +11,7 @@ currentKernel=$(uname -r)
 if [[ "$currentKernel" != "$latestKernel" ]]; then
     exit_with_error "Upgrade kernel $currentKernel -> $latestKernel (latest) before installing NVIDIA drivers"
 fi
-sudo dnf install -y akmod-nvidia || exit 1
+sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda || exit 1
 log_success "Finished installing repackaged NVIDIA driver from RPMFusion"
 log_info "Getting the CUDA libraries from NVIDIA's own packages..."
 sudo mkdir -p /opt/MagAOX/vendor/cuda_rpm
@@ -20,10 +20,13 @@ sudo chmod g+ws /opt/MagAOX/vendor/cuda_rpm
 cd /opt/MagAOX/vendor/cuda_rpm
 rpmFile=cuda-repo-fedora42-13-0-local-13.0.2_580.95.05-1.x86_64
 rpmVers=13.0.2
-package=cuda-toolkit-13
 _cached_fetch https://developer.download.nvidia.com/compute/cuda/$rpmVers/local_installers/$rpmFile.rpm $rpmFile.rpm
 if ! rpm -q $rpmFile; then
     sudo rpm -i $rpmFile.rpm
 fi
-sudo dnf -y install $package || exit 1
+if [[ $MAGAOX_ROLE == AOC || $MAGAOX_ROLE == ROC || $MAGAOX_ROLE == COC ]]; then
+    sudo dnf -y install cuda-toolkit-13 || exit 1
+else
+    sudo dnf -y install cuda-compiler-13-0 cuda-libraries-13-0 cuda-libraries-dev-13-0 || exit 1
+fi
 log_success "Installed CUDA libraries!"
