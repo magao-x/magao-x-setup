d25855f3289d5270dd60efb521e99d03eea1047d
Joseph D. Long <me@joseph-long.com>
2025-11-27 22:42:13 -0300

    Damn not --yes again

--- jupyterhub_config_minimal.py
+++ jupyterhub_config_minimal.py
@@ -15,9 +15,12 @@ c.JupyterHub.cookie_secret_file = '/var/lib/jupyterhub/jupyterhub_cookie_secret'
 c.ConfigurableHTTPProxy.pid_file = '/run/jupyterhub/jupyterhub-proxy.pid'
 # SystemD unit ensures creation of this dir for persistent state
 c.JupyterHub.db_url = 'sqlite:////var/lib/jupyterhub/jupyterhub.sqlite'
+# "Due to longstanding problems in the session lifecycle, this is now disabled by default"
+# ok well
+c.PAMAuthenticator.open_sessions = True
 # Only specified users may log in to JupyterHub
 c.PAMAuthenticator.allowed_groups = {'jupyterhub', 'xwcl-admin'}
 # Which groups should get admin access
 c.PAMAuthenticator.admin_groups = {'wheel', 'xwcl-admin'}
 ## Path to the notebook directory for the single-user server.
-c.Spawner.notebook_dir = '/srv/aoc/home/{username}'
+c.Spawner.notebook_dir = '/home/{username}'
