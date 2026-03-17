07a6bc55e5a7043c59bee0e8d3d4b867df58ec02
Joseph D. Long <me@joseph-long.com>
2026-03-17 13:41:36 -0400

    Update fstab-wrangling for CACTI support and missing optional fields for /milk/shm

--- steps/configure_nfs.sh
+++ steps/configure_nfs.sh
@@ -12,16 +12,18 @@ else
     exit_with_error "Amend the NFS config script to include the nfsServiceUnit value for $ID"
 fi
 
-if [[ $MAGAOX_ROLE == RTC || $MAGAOX_ROLE == ICC || $MAGAOX_ROLE == AOC ]]; then
+if [[ $MAGAOX_ROLE == RTC || $MAGAOX_ROLE == ICC || $MAGAOX_ROLE == AOC || $MAGAOX_ROLE == TIC || $MAGAOX_ROLE == TOC ]]; then
     sudo systemctl enable --now $nfsServiceUnit || exit 1
     sudo systemctl start $nfsServiceUnit || exit 1
-    if command -v ufw; then
-        sudo ufw allow from 192.168.0.0/24 to any port nfs || exit 1
-    fi
+
     # /etc/systemd/system/nfs-server.service.d/override.conf
     overridePath="/etc/systemd/system/${nfsServiceUnit}.d/override.conf"
     sudo mkdir -p "/etc/systemd/system/${nfsServiceUnit}.d/" || exit 1
     echo -e "[Service]\nTimeoutStopSec=5s" | sudo tee $overridePath || exit 1
+fi
+
+# MagAO-X only (CACTI below)
+if [[ $MAGAOX_ROLE == RTC || $MAGAOX_ROLE == ICC || $MAGAOX_ROLE == AOC ]]; then
     exportHosts=""
     for host in aoc rtc icc; do
         if [[ ${host,,} != ${MAGAOX_ROLE,,} ]]; then
@@ -60,7 +62,7 @@ if [[ $MAGAOX_ROLE == RTC || $MAGAOX_ROLE == ICC || $MAGAOX_ROLE == AOC ]]; then
                 mountPath=/srv/$host/opt/MagAOX/$magaoxSubfolder
                 sudo mkdir -vp $mountPath || exit 1
                 if ! grep -q "$mountPath" /etc/fstab; then
-                    echo "$host:$hostPath $mountPath	nfs	rw,noauto,x-systemd.automount,nofail,x-systemd.device-timeout=10s,soft,timeo=30	0 0" | sudo tee -a /etc/fstab || exit 1
+                    echo "$host:$hostPath $mountPath	nfs	rw,noauto,x-systemd.automount,nofail,soft,timeo=30	0 0" | sudo tee -a /etc/fstab || exit 1
                 fi
             fi
         done
@@ -69,15 +71,52 @@ if [[ $MAGAOX_ROLE == RTC || $MAGAOX_ROLE == ICC || $MAGAOX_ROLE == AOC ]]; then
         aocHomeMountPath=/home
         sudo mkdir -p /srv/aoc/home || exit 1
         if ! grep -q /srv/aoc/home /etc/fstab; then
-            echo "aoc:/home $aocHomeMountPath	nfs	rw,noauto,x-systemd.automount,nofail,x-systemd.device-timeout=10s,soft,timeo=30	0 0" | sudo tee -a /etc/fstab || exit 1
+            echo "aoc:/home $aocHomeMountPath	nfs	rw,noauto,x-systemd.automount,nofail,soft,timeo=30	0 0" | sudo tee -a /etc/fstab || exit 1
         fi
     fi
     if [[ $MAGAOX_ROLE != AOC ]]; then
         backupsMountPath=/srv/aoc/mnt/backups
         sudo mkdir -p $backupsMountPath || exit 1
         if ! grep -q $backupsMountPath /etc/fstab; then
-            echo "aoc:/mnt/backups $backupsMountPath	nfs	ro,noauto,x-systemd.automount,nofail,x-systemd.device-timeout=10s,soft,timeo=30	0 0" | sudo tee -a /etc/fstab || exit 1
+            echo "aoc:/mnt/backups $backupsMountPath	nfs	ro,noauto,x-systemd.automount,nofail,soft,timeo=30	0 0" | sudo tee -a /etc/fstab || exit 1
         fi
     fi
+fi
+# CACTI
+if [[ $MAGAOX_ROLE == TIC || $MAGAOX_ROLE == TOC ]]; then
+    exportHosts=""
+    for host in tic toc; do
+        if [[ ${host,,} != ${MAGAOX_ROLE,,} ]]; then
+            exportHosts="$host(rw,sync) $exportHosts"
+        fi
+    done
+    if [[ $MAGAOX_ROLE == TIC ]]; then
+        exportDataLine="/home      $exportHosts"
+    else
+        exportDataLine="/data      $exportHosts"
+    fi
+    if ! grep -q "$exportDataLine" /etc/exports; then
+        echo "$exportDataLine" | sudo tee -a /etc/exports || exit 1
+        sudo exportfs -a || exit 1
+        sudo systemctl reload $nfsServiceUnit || exit 1
+    fi
+
+    # every host mounts the other hosts' data folders
+    for host in tic toc; do
+        for magaoxSubfolder in cacao logs rawimages telem calib; do
+            if [[ ${host,,} != ${MAGAOX_ROLE,,} ]]; then
+                if [[ $host == aoc ]]; then
+                    hostPath=/home/data/$magaoxSubfolder
+                else
+                    hostPath=/data/$magaoxSubfolder
+                fi
+                mountPath=/srv/$host/opt/MagAOX/$magaoxSubfolder
+                sudo mkdir -vp $mountPath || exit 1
+                if ! grep -q "$mountPath" /etc/fstab; then
+                    echo "$host:$hostPath $mountPath	nfs	rw,noauto,x-systemd.automount,nofail,soft,timeo=30	0 0" | sudo tee -a /etc/fstab || exit 1
+                fi
+            fi
+        done
+    done
 fi
 sudo systemctl daemon-reload || exit_with_error "SystemD couldn't reload"
