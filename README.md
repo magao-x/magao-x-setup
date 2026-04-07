a1c621a555ed190bf281aaef6d32521588d4ab97
Joseph D. Long <me@joseph-long.com>
2026-04-07 13:05:45 -0400

    Add check for available RAM before compiling Flatbuffers

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 The scripts in this folder are runnable documentation for the setup of the MagAO-X software system on new hardware (to the extent it is possible to automate).
 
 For instrument computer setup, consult the [computer setup section](https://magao-x.org/docs/handbook/compute/computer_setup/computer_setup.html) of the [MagAO-X Handbook](https://magao-x.org/docs/handbook/).
+
+## Container Build Memory
+
+Container builds compile several large C/C++ dependencies, including flatbuffers. If the Podman machine has less than 4 GB RAM, the build may be killed by the OOM killer.
+
+Set Podman machine memory to at least 4096 MB; 8192 MB is recommended:
+
+```bash
+podman machine set --memory 8192
+```
--- steps/install_flatbuffers.sh
+++ steps/install_flatbuffers.sh
@@ -3,9 +3,31 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 set -uo pipefail
 FLATBUFFERS_VERSION="23.5.26"
+MIN_RAM_BYTES=$((4 * 1024 * 1024 * 1024))
 #
 # Flatbuffers
 #
+
+# Compiling flatbuffers can OOM on low-memory Podman machines.
+memTotalKb=$(awk '/MemTotal:/ {print $2}' /proc/meminfo)
+if [[ -z "$memTotalKb" ]]; then
+    exit_with_error "Could not determine system memory from /proc/meminfo"
+fi
+availableBytes=$((memTotalKb * 1024))
+
+# Respect container memory limits when present (cgroup v2).
+if [[ -r /sys/fs/cgroup/memory.max ]]; then
+    cgroupLimit=$(cat /sys/fs/cgroup/memory.max)
+    if [[ "$cgroupLimit" != "max" && "$cgroupLimit" =~ ^[0-9]+$ && "$cgroupLimit" -lt "$availableBytes" ]]; then
+        availableBytes=$cgroupLimit
+    fi
+fi
+
+if [[ "$availableBytes" -lt "$MIN_RAM_BYTES" ]]; then
+    availableMb=$((availableBytes / 1024 / 1024))
+    exit_with_error "Flatbuffers build requires at least 4096 MB RAM; detected ${availableMb} MB. Increase Podman machine memory (e.g. to 8192 MB)."
+fi
+
 cd /opt/MagAOX/vendor || exit 1
 FLATBUFFERS_DIR="flatbuffers-$FLATBUFFERS_VERSION"
 if [[ ! -d $FLATBUFFERS_DIR ]]; then
