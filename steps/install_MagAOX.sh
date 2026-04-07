b4dff23cf57a806fabcd452af30c2fbe30095a58
Joseph D. Long <me@joseph-long.com>
2026-04-07 14:34:45 -0400

    Fix for unset PKG_CONFIG_PATH error

--- steps/install_MagAOX.sh
+++ steps/install_MagAOX.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
-set -uo pipefail
 # Make sure we get all the new pkg-config files
+# before enabling abort-on-unset in case PKG_CONFIG_PATH
+# is unset
 export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:${PKG_CONFIG_PATH}"
+set -uo pipefail
 # Uses Software Collections package for GCC 14
 if [[ -e /opt/rh/gcc-toolset-14/enable ]]; then
     source /opt/rh/gcc-toolset-14/enable
