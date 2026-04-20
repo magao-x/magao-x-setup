916439d2146545c9407b2dece5c0e75511ce6909
Joseph D. Long <me@joseph-long.com>
2026-04-20 11:23:36 -0400

    Don't try and enable the liveness service because timer does that

--- steps/configure_vizzy_liveness.sh
+++ steps/configure_vizzy_liveness.sh
@@ -11,9 +11,11 @@ UNIT_PATH=/etc/systemd/system
 for service_unit in vizzy-liveness.service vizzy-liveness.timer vizzy-notify-power-state.service; do
     cp -v $DIR/../systemd_units/$service_unit $UNIT_PATH/$service_unit || exit 1
     log_success "Installed $service_unit to $UNIT_PATH"
-    systemctl daemon-reload || exit 1
-    systemctl enable --now $service_unit || true
 done
+systemctl daemon-reload || exit 1
+
+systemctl enable --now vizzy-liveness.timer || true
+systemctl enable --now vizzy-notify-power-state.timer || true
 
 cp -v $DIR/../systemd_units/vizzy-liveness.sh /usr/local/bin/
 chmod u=rwx,g=rx,o=rx /usr/local/bin/vizzy-liveness.sh
