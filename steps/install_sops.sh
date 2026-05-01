920f808cb4b555674abd99dbb2b02fc3de0564b6
Irina Stefan <144726379+stefi07@users.noreply.github.com>
2026-05-01 15:54:00 +0100

    Update sops installation script for Ubuntu support

--- steps/install_sops.sh
+++ steps/install_sops.sh
@@ -6,10 +6,21 @@ cd /opt/MagAOX/vendor || exit 1
 mkdir -p sops || exit 1
 cd sops || exit 1
 release=3.10.2
-downloadFile=sops-${release}.rpm
-if [[ ! -e $downloadFile ]]; then
-    _cached_fetch https://github.com/getsops/sops/releases/download/v${release}/sops-${release}-1.$(uname -m).rpm $downloadFile || exit 1
-fi
-if ! command -v sops; then
-    $SUDO rpm -i $downloadFile || exit 1
+source /etc/os-release
+if [[ $ID == "ubuntu" ]]; then
+    downloadFile=sops_${release}_$(dpkg --print-architecture).deb
+    if [[ ! -e $downloadFile ]]; then
+        _cached_fetch https://github.com/getsops/sops/releases/download/v${release}/sops_${release}_$(dpkg --print-architecture).deb $downloadFile || exit 1
+    fi
+    if ! command -v sops; then
+        $SUDO dpkg -i $downloadFile || exit 1
+    fi
+else
+    downloadFile=sops-${release}.rpm
+    if [[ ! -e $downloadFile ]]; then
+        _cached_fetch https://github.com/getsops/sops/releases/download/v${release}/sops-${release}-1.$(uname -m).rpm $downloadFile || exit 1
+    fi
+    if ! command -v sops; then
+        $SUDO rpm -i $downloadFile || exit 1
+    fi
 fi
